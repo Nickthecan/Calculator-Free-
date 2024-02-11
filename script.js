44d8e7f5326695fb6e3ef2b1ed0357295efbccdb
Nicholas Amancio <nick.amancio44@gmail.com>
2024-02-10 17:51:19 -0800

    added the calculator app

---
+++ script.js
@@ -0,0 +1,11 @@
+function displayNumber(number) {
+    document.querySelector('.display').textContent += number
+}
+
+function buy() {
+    alert('This operator is available in the premium version of the app')
+}
+
+function equal() {
+    alert("Calculator says 'Hello World!'")
+}
